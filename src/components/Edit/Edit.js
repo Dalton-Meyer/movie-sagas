586bcbc6ef30daafe8fde4ef0f9a0ab1cebadca9
Dalton Meyer <dalton.meyer94@gmail.com>
2020-06-29 08:42:45 -0500

    set up a confirmation to edit movie

--- src/components/Edit/Edit.js
+++ src/components/Edit/Edit.js
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Route, Link } from "react-router-dom";
+import { Route, Link, Redirect } from "react-router-dom";
 import { withRouter } from "react-router-dom";
+import { confirmAlert } from 'react-confirm-alert'; // Import
+import 'react-confirm-alert/src/react-confirm-alert.css'; // Import css
 
 class Edit extends Component {
     // setting local state for the inputs
     state = {
         title: '',
         description: '',
-        id: 0
+        id: 0,
+        redirect: false
     }
 
     componentDidMount(){
@@ -47,14 +50,37 @@ class Edit extends Component {
     handleSubmit = () => {
       if (this.state.title !== '' && this.state.description !== '') {
         this.props.dispatch({type:'EDIT_MOVIE', payload: this.state}) 
+        this.setState({redirect: true})
       }else{
         alert('please make sure input are not empty')
       }
     }
+    submit = () => {
+        confirmAlert({
+          title: 'Confirm to submit',
+          message: 'Are you sure you want to edit this?.',
+          buttons: [
+            {
+              label: 'Yes',
+              onClick: () => this.handleSubmit()
+            },
+            {
+              label: 'No',
+              onClick: () => alert('Click No')
+            }
+          ]
+        });
+      };
+      renderRedirect = () => {
+        if (this.state.redirect) {
+          return <Redirect to = "/details" />
+        }
+      }
 
   render() {
     return (
       <div className='edit'>
+          {this.renderRedirect()}
           <h3>Title</h3>
         <input type="text" className="titleInput" value={this.state.title} onChange={(event)=>this.handleChange("title",event)}/>
         <br/>
@@ -63,7 +89,7 @@ class Edit extends Component {
         <br/>
         <Link to="/details"><button className="btn btn-primary btn-lg">Back to Details</button></Link> 
         <br/> <br/>
-        <Link to="/details"><button className="btn btn-primary btn-lg" onClick={()=>this.handleSubmit()}>Save</button></Link>
+        <button className="btn btn-primary btn-lg" onClick={()=>this.submit()}>Save</button>
         
       </div>
     )
